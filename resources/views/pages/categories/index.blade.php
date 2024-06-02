c569936cd2d200c87d5206a5a3aa31a67fc4f4bb
Fahriz Aziz <63221929+fahrizaziz@users.noreply.github.com>
2024-06-02 19:08:44 +0700

    dashboard products & category

--- resources/views/pages/categories/index.blade.php
+++ resources/views/pages/categories/index.blade.php
@@ -11,14 +11,14 @@
     <div class="main-content">
         <section class="section">
             <div class="section-header">
-                <h1>Categories</h1>
+                <h1>Category</h1>
                 <div class="section-header-button">
                     <a href="{{ route('categories.create') }}" class="btn btn-primary">Add New</a>
                 </div>
                 <div class="section-header-breadcrumb">
                     <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-                    <div class="breadcrumb-item"><a href="#">Categories</a></div>
-                    <div class="breadcrumb-item">All Categories</div>
+                    <div class="breadcrumb-item"><a href="#">Category</a></div>
+                    <div class="breadcrumb-item">All Category</div>
                 </div>
             </div>
             <div class="section-body">
@@ -27,27 +27,16 @@
                         @include('layouts.alert')
                     </div>
                 </div>
-                <h2 class="section-title">Categories</h2>
-                <p class="section-lead">
-                    You can manage all Categories, such as editing, deleting and more.
-                </p>
 
 
                 <div class="row mt-4">
                     <div class="col-12">
                         <div class="card">
                             <div class="card-header">
-                                <h4>All Posts</h4>
+                                <h4>All category</h4>
                             </div>
                             <div class="card-body">
-                                <div class="float-left">
-                                    <select class="form-control selectric">
-                                        <option>Action For Selected</option>
-                                        <option>Move to Draft</option>
-                                        <option>Move to Pending</option>
-                                        <option>Delete Pemanently</option>
-                                    </select>
-                                </div>
+
                                 <div class="float-right">
                                     <form method="GET" action="{{ route('categories.index') }}">
                                         <div class="input-group">
@@ -66,7 +55,8 @@
                                         <tr>
 
                                             <th>Name</th>
-                                            <th>Created At</th>
+
+                                            <th>Create At</th>
                                             <th>Action</th>
                                         </tr>
                                         @foreach ($categories as $category)
@@ -74,6 +64,7 @@
 
                                                 <td>{{ $category->name }}
                                                 </td>
+
                                                 <td>{{ $category->created_at }}</td>
                                                 <td>
                                                     <div class="d-flex justify-content-center">
